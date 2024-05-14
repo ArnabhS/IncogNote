336ac58655673e4686a1378d52fb228fb7e38402
Arnabh <sinhaarnabh888@gmail.com>
2024-05-14 11:38:02 +0530

    signup page added

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,30 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
+type ConnectionObject = {
+  isConnected?: number;
+};
 
-type ConnectionObject={
-    isConnected?: number
-}
-
-const connection: ConnectionObject = {}
+const connection: ConnectionObject = {};
 
 async function dbConnect(): Promise<void> {
-    if(connection.isConnected){
-        console.log("db already connected");
-        return;
-    }
-    try{
-       const db = await mongoose.connect(process.env.MONGODB_URI || '',{})
-       console.log(db);
+  // Check if we have a connection to the database or if it's currently connecting
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
+  }
 
-       connection.isConnected = db.connections[0].readyState;
+  try {
+    // Attempt to connect to the database
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
 
-       console.log("Db connected successfully")
-    }
-    catch(error){
-        console.log("db connection failed", error)
+    connection.isConnected = db.connections[0].readyState;
 
-        process.exit(1)
-    }
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+
+    // Graceful exit in case of a connection error
+    process.exit(1);
+  }
 }
 
 export default dbConnect;
